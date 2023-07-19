e22a6f92596071d4e0c187d2d4067c6893d8f6ea
Vikas Sharma <vkassharma19492@gmail.com>
2023-07-20 02:18:54 +0530

    It's working

--- VoiceRec3/App.tsx
+++ VoiceRec3/App.tsx
@@ -1,19 +1,103 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
-import { View, Text, Button, StyleSheet, Alert } from "react-native";
-import Voice from "@react-native-voice/voice";
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button, StyleSheet, Alert} from 'react-native';
+import Voice, {
+  SpeechRecognizedEvent,
+  SpeechResultsEvent,
+  SpeechErrorEvent,
+} from '@react-native-voice/voice';
 
 const App = () => {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Voice Rec-Version3</Text>
-        <Text style={styles.title1}>
-          Response Result: I am hoping the result should be a big text array
-        </Text>
-        <Button title="Press Me" onPress={() => {Alert.alert("Button Clicked Successfully")}}/>
-      </View>
-    );
-}
+  const [recognized, setRecognized] = useState('');
+  const [end, setEnd] = useState('');
+  const [started, setStarted] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechRecognized = onSpeechRecognized;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const onSpeechStart = (e: any) => {
+    console.log('onSpeechStart: ', e);
+    setStarted('√');
+  };
+
+  const onSpeechRecognized = (e: SpeechRecognizedEvent) => {
+    console.log('onSpeechRecognized: ', e);
+    setRecognized('√');
+  };
+
+  const onSpeechEnd = (e: any) => {
+    console.log('onSpeechEnd: ', e);
+    setEnd('√');
+  };
+
+  const onSpeechResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechResults: ', e);
+    setResults(e.value);
+  };
+
+  const _startRecognizing = async () => {
+    _clearState();
+    try {
+      await Voice.start('en-US');
+      console.log('called start');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    _clearState();
+  };
+
+  const _clearState = () => {
+    setRecognized('');
+    setEnd('');
+    setStarted('');
+    setResults([]);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Voice Rec-Version3</Text>
+      <Text style={styles.title1}>{`Started: ${started}`}</Text>
+      <Text style={styles.title1}>{`Recognized: ${recognized}`}</Text>
+      <Text style={styles.title1}>Results</Text>
+      {results.map((result, index) => {
+        return (
+          <Text key={`result-${index}`} style={styles.title1}>
+            {result}
+          </Text>
+        );
+      })}
+      <Text style={styles.title1}>{`End: ${end}`}</Text>
+      <Button title="Start Rec" onPress={_startRecognizing} />
+      <Button title="Stop Rec" onPress={_stopRecognizing} />
+      <Button title="Destroy Rec" onPress={_destroyRecognizer} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
