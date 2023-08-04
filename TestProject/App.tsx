ba98bdaddaf63b2afb8515dd522e48c941feab30
Vikas Sharma <vkassharma19492@gmail.com>
2023-08-04 14:23:37 +0530

    detect this issue

--- TestProject/App.tsx
+++ TestProject/App.tsx
@@ -5,23 +5,28 @@ import {Text, Button, View, Alert} from 'react-native';
 const App = () => {
   const testBtn = useRef();
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (testBtn.current) {
-        testBtn.current.props.onPress();
-      }
-    }, 5000);
+    if (testBtn.current) {
+      // testBtn.current.props.onPress();
+      testBtn.current._internalFiberInstanceHandleDEV.pendingProps.children[1].props.onPress();
+      // console.log(
+      //   'Working',
+      //   testBtn.current._internalFiberInstanceHandleDEV.pendingProps.children[1].props.onPress()
+      // );
+    }
+    // const interval = setInterval(() => {
+    //   if (testBtn.current) {
+    //     // testBtn.current.props.onPress();
+    //     console.log('Working', testBtn.current._internalFiberInstanceHandleDEV);
+    //   }
+    // }, 5000);
 
-    return () => clearInterval(interval);
+    // return () => clearInterval(interval);
   }, []);
 
   return (
-    <View>
+    <View ref={testBtn}>
       <Text>Auto calling Button</Text>
-      <Button
-        title="Button"
-        ref={testBtn}
-        onPress={() => Alert.alert('Button Clicked')}
-      />
+      <Button title="Button" onPress={() => Alert.alert('Button Clicked')} />
     </View>
   );
 }
