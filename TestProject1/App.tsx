ca338e58f767bd6e74bdd9e06a8d46bf0aea8089
Vikas Sharma <vkassharma19492@gmail.com>
2023-08-10 01:52:28 +0530

    adding/using custom view component using forwardRef

--- TestProject1/App.tsx
+++ TestProject1/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import {useRef, useEffect} from 'react';
+import React, {useRef, useEffect} from 'react';
 import {Text, Button, View, Alert} from 'react-native';
 
-const App = () => {
-  const testBtn = useRef();
+const MyButton = React.forwardRef((props, ref) => {
+  const buttonRef = useRef();
+
   useEffect(() => {
-    if (testBtn.current) {
-      testBtn.current._internalFiberInstanceHandleDEV.pendingProps.children[1].props.onPress();
-      console.log(
-        'Working',
-        testBtn.current._internalFiberInstanceHandleDEV.memoizedProps
-          .children[1].props.onPress,
-      );
+    if (buttonRef.current) {
+      buttonRef.current.props.onPress();
     }
-  }, []);
+  }, [props.onPress]);
+
+  return <Button ref={buttonRef} {...props} />;
+});
+
+const App = () => {
 
   return (
-    <View ref={testBtn}>
-      <Text>Auto calling Button</Text>
-      <Button title="Button" onPress={() => Alert.alert('Button Clicked')} />
+    <View>
+      <Text>Auto calling Buttons</Text>
+      <MyButton
+        title="Button 1"
+        onPress={() => Alert.alert('Button 1 Clicked')}
+      />
+      <MyButton
+        title="Button 2"
+        onPress={() => Alert.alert('Button 2 Clicked')}
+      />
+      <MyButton
+        title="Button 3"
+        onPress={() => Alert.alert('Button 3 Clicked')}
+      />
     </View>
   );
 };
